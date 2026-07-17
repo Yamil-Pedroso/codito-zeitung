2d07e2a9bb03c922ddadbe14b78b8b28b6c74abb
Yamil Pedroso <yamirovinci@gmail.com>
2026-07-17 15:36:26 +0200

    New links connected to the newspaper's platforms

--- frontend/src/Data/news.ts
+++ frontend/src/Data/news.ts
@@ -1,9 +1,9 @@
 export { articles } from './generatedArticles'
 
 export const sourceDescriptions = [
-  { name: 'WOZ', mark: 'WOZ', detail: 'Gesellschaft, Politik und Kultur', tint: '#8a5d2d' },
-  { name: 'SRF News', mark: 'SRF', detail: 'Aktuelles aus dem Service public und der Schweiz', tint: '#9d2b27' },
-  { name: 'ETH Zürich', mark: 'ETH', detail: 'Wissenschaft, Technologie und Forschung', tint: '#4d6659' },
-  { name: 'NZZ', mark: 'NZZ', detail: 'Analysen, Wirtschaft und Weltgeschehen', tint: '#293f4c' },
-  { name: 'WWF Schweiz', mark: 'WWF', detail: 'Umwelt, Klima, Biodiversität und nachhaltiges Leben', tint: '#4f6f32' },
+  { name: 'WOZ', mark: 'WOZ', detail: 'Gesellschaft, Politik und Kultur', tint: '#8a5d2d', url: 'https://www.woz.ch' },
+  { name: 'SRF News', mark: 'SRF', detail: 'Aktuelles aus dem Service public und der Schweiz', tint: '#9d2b27', url: 'https://www.srf.ch/news' },
+  { name: 'ETH Zürich', mark: 'ETH', detail: 'Wissenschaft, Technologie und Forschung', tint: '#4d6659', url: 'https://www.ethz.ch' },
+  { name: 'NZZ', mark: 'NZZ', detail: 'Analysen, Wirtschaft und Weltgeschehen', tint: '#293f4c', url: 'https://www.nzz.ch' },
+  { name: 'WWF Schweiz', mark: 'WWF', detail: 'Umwelt, Klima, Biodiversität und nachhaltiges Leben', tint: '#4f6f32', url: 'https://www.wwf.ch' },
 ] as const
